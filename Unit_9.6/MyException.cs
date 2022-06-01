790c01179289d8f2308fcc43218b38dfa03e9d48
rodion3000 <rodion.papizh@mail.ru>
2022-06-01 15:29:46 +0500

    Добавьте файлы проекта.

---
+++ Unit_9.6/MyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_9._6
+{
+    public class MyException:Exception
+    {
+        public MyException()
+        {
+            Exception exception1 = new Exception("Произошло новое исключение 1");
+            exception1.Data.Add("Создание исключения:", DateTime.Now);
+            exception1.HelpLink = "Новое исключение 1";
+            
+        }
+    }
+}
---
+++ Unit_9.6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_9._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyException myException = new MyException();
+            ArgumentException argumentException = new ArgumentException();
+            FileNotFoundException fileException = new FileNotFoundException();
+            IndexOutOfRangeException indexOutOfRangeException = new IndexOutOfRangeException();
+            DivideByZeroException divide = new DivideByZeroException();
+            
+            Exception[] ExceptionMassive = new Exception[] {myException,argumentException,fileException,indexOutOfRangeException,divide};
+            foreach(Exception x in ExceptionMassive)
+            {
+                try
+                {
+                    throw x;
+
+
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FileNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (MyException ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+              
+            }
+            
+        }
+    }
+}
+
